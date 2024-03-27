08da3830756a22a55a6eeeb786836653e77f1b23
uchihiro <tohchi4122@gmail.com>
2024-03-27 23:24:17 +0900

    Mainクラス、Gameクラスのひな形を作成

---
+++ game.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Game
+  attr_reader :first_player, :second_player
+
+  def initialize(first_player, second_player)
+    @first_player = first_player
+    @second_player = second_player
+  end
+
+  def play
+  end
+end
